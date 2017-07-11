7e75258e8d074f333544887d77b84534753aa3ca
eleves <eleves@mac404inf-prof.local>
2017-07-11 16:26:47 -0400

    v1

--- Weyller/ViewController.swift
+++ Weyller/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    @IBOutlet weak var tableView: UITableView!
+    let aDict = ["Key1" : "Value1", "Key2" : "Value2", "Key3" : "Value3"]
+    var arrayOfKeys: [String] = []
 
+    @IBOutlet weak var aLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        parseDict()
+    }
+    
+    func parseDict() {
+        for (key, _) in aDict {
+            arrayOfKeys.append(key)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //---------------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableView.backgroundColor = UIColor.clear
+        return arrayOfKeys.count
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier:"proto")
+        cell.textLabel!.text = arrayOfKeys[indexPath.row]
+        cell.textLabel?.textColor = UIColor.black
+        cell.backgroundColor = UIColor.clear
+        return cell
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath as IndexPath)!
+        selectedCell.contentView.backgroundColor = UIColor.darkGray
+        let aKey = arrayOfKeys[indexPath.row]
+        aLabel.text = aDict[aKey]
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+    //---------------------
 
 }
 
